e29bda97457e5f2345f13a7368968afeda940cbb
Mahmoud Hboubati <mahmoud.hboubati@emaratech.ae>
2019-09-24 20:39:59 +0400

    feat(*): add ninject

--- Core/Providers/Users/UserProvider.cs
+++ Core/Providers/Users/UserProvider.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Core.Providers.Users
 {
-    public class UserProvider
+    public class UserProvider : IUserProvider
     {
     }
 }
---
+++ UserRegistration/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,47 @@
+﻿using Core.Providers.Users;
+using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+using Ninject;
+using Ninject.Web.Common;
+using Ninject.Web.Common.WebHost;
+using System;
+using System.Web;
+using UserRegistration.App_Start;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(NinjectWebCommon), "Stop")]
+
+namespace UserRegistration.App_Start
+{
+    public static class NinjectWebCommon
+    {
+        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+
+        public static void Start()
+        {
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+            bootstrapper.Initialize(CreateKernel);
+        }
+
+        public static void Stop()
+        {
+            bootstrapper.ShutDown();
+        }
+
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+            kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+
+            kernel.Bind<IUserProvider>().To<UserProvider>();
+
+            RegisterServices(kernel);
+            return kernel;
+        }
+        private static void RegisterServices(IKernel kernel)
+        {
+            //kernel.Bind<IRepo>().ToMethod(ctx => new Repo("Ninject Rocks!"));
+        }
+    }
+}

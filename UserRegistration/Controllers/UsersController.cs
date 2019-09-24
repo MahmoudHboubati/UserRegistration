015e135ee9f316731fdf183a58693f99288c97a0
Mahmoud Hboubati <mahmoud.hboubati@emaratech.ae>
2019-09-24 20:11:25 +0400

    first commit

---
+++ UserRegistration/Controllers/UsersController.cs
@@ -0,0 +1,105 @@
+﻿using Core.Providers.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserRegistration.Models.Users;
+
+namespace UserRegistration.Controllers
+{
+    public class UsersController : Controller
+    {
+        readonly IUserProvider provider;
+
+        public UsersController(IUserProvider provider)
+        {
+            this.provider = provider;
+        }
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            UserIndexViewModel model = new UserIndexViewModel(provider);
+            return View(model);
+        }
+
+        public ActionResult IndexListPartial()
+        {
+            UserIndexViewModel model = new UserIndexViewModel(provider);
+            return PartialView(model);
+        }
+
+        // GET: Users/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Users/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ UserRegistration/Models/Users/UserIndexViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Core.Providers.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UserRegistration.Models.Users
+{
+    public class UserIndexViewModel
+    {
+        private readonly IUserProvider provider;
+
+        public UserIndexViewModel(IUserProvider provider)
+        {
+            this.provider = provider;
+        }
+
+        public void LoadList()
+        {
+
+        }
+
+    }
+}

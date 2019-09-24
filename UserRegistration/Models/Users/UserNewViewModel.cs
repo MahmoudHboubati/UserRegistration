c397ae4e15ff4fee62a349bd48aca104ffe736c1
Mahmoud Hboubati <mahmoud.hboubati@emaratech.ae>
2019-09-24 23:22:24 +0400

    feat(*): add registration form

--- UserRegistration/Models/Users/UserNewViewModel.cs
+++ UserRegistration/Models/Users/UserNewViewModel.cs
@@ -3,12 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Core.Providers.Users;
+using System.ComponentModel.DataAnnotations;
 
 namespace UserRegistration.Models.Users
 {
     public class UserNewViewModel
     {
         private IUserProvider provider;
+
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(32)]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
         public UserNewViewModel()
         {
         }
